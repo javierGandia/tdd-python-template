1997e75d4efea368c8913ed1158140fb51292cbd
javierGandia <jgandiamartin@gmail.com>
2022-02-25 12:36:43 +0100

    añadiendo resta

---
+++ tests/test_resta.py
@@ -0,0 +1,9 @@
+from app.operaciones import resta
+
+
+class TestClass:
+    def test_suma(self):
+        assert resta(5, 4) == 1
+        assert resta(4, 3) == 1
+        assert resta(8, 4) == 4
+        assert resta(10, 5) == 5
